7e100a0186115f5fb1a5cd3538e7e885817513ae
DrewMcCarthy <drew.r.mccarthy@gmail.com>
2019-08-15 00:35:04 -0400

    new autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,29 @@ const gulp = require('gulp');
 const sass = require('gulp-sass'); 
 const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps'); 
-const postcss = require('gulp-postcss'); 
-const autoprefixer = require('autoprefixer'); 
-const notify = require('gulp-notify');
+const autoprefixer = require('gulp-autoprefixer'); 
+var plumber = require('gulp-plumber');
 
 sourceDir = './site/'; 
 destDir = './site/'; 
 stylesSrc = sourceDir + 'scss/**/*.scss'; 
 stylesDest = destDir + 'css/';
 
-// function handleErrors(error){
-//     notify().write(error);
-//     console.log(error);
-// }
+function handleErrors(error){
+    console.log(error);
+}
 
 function style(){
     return gulp.src(stylesSrc)
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(postcss([autoprefixer()]))
+        .pipe(autoprefixer('last 2 versions'))
         .pipe(sourcemaps.write('.')) // Needs to be before dest()
         .pipe(gulp.dest(stylesDest))
         .pipe(browserSync.stream())
+        .pipe(plumber({
+            errorHandler: handleErrors
+        }))
 }
 
 function watch(){
