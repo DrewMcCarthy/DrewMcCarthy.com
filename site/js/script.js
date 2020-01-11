9e2f81fd4c5a3fb581f4a2c8b113cafb1fed8a5c
DrewMcCarthy <drew.r.mccarthy@gmail.com>
2020-01-11 13:24:25 -0500

    added form validation

--- site/js/script.js
+++ site/js/script.js
@@ -1,5 +1,27 @@
 
-async function contactSend() {
+async function submitForm(event) {
+    console.log(event);
+    validateForm(event);
+}
+
+async function validateForm(event) {
+    if (!event.target.form) {
+        return;
+    }
+    
+    var form = event.target.form;
+    form.classList.add("was-validated");
+
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        event.stopPropagation();
+    } else {
+        contactSend(event);
+    }
+}
+
+// Send form to API
+async function contactSend(event) {
     const uri = "https://drewmccarthy.com/api/contact";
     var contactName = document.getElementById("contact-name");
     var contactEmail = document.getElementById('contact-email');
